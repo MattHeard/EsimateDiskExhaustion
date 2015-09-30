f5ee2cebc16a28e61324b4070b6827b647a51893
Matthew Heard <matt.heard@oracle.com>
2015-10-01 07:53:54 +1300

    Separate disk space from process rates

--- estimate.py
+++ estimate.py
@@ -3,8 +3,11 @@
 from sys import stdin
 
 def estimateConf(conf):
-    confElements = conf.split(sep=" ")
-    print(confElements)
+    confElements = [int(x) for x in conf.split(sep=" ")]
+    disk = confElements[0]
+    print(disk)
+    procRates = confElements[1:]
+    print(procRates)
 
 def estimateConfsFromInput():
     for line in stdin:
