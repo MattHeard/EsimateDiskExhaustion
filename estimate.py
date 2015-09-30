85b789a256e3785836d2c6dab87267a3d18a3fc2
Matthew Heard <matt.heard@oracle.com>
2015-10-01 07:42:44 +1300

    Split lines

--- estimate.py
+++ estimate.py
@@ -6,8 +6,10 @@ def estimateConf(conf):
     print(conf)
 
 def estimateConfsFromInput():
-    for conf in stdin:
-        estimateConf(conf)
+    for line in stdin:
+        confs = line.splitlines()
+        for conf in confs:
+            estimateConf(conf)
 
 if __name__ == "__main__":
     estimateConfsFromInput()
